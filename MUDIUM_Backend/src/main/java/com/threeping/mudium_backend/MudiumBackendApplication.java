837fa288ae00dc32ad4e0bc6e4898130fafad4e6
dongkh9 <dongkh99@gamil.com>
2024-10-10 10:16:44 +0900

    feat: 프로젝트 생성
    초기 프로젝트 구조 생성중

---
+++ MUDIUM_Backend/src/main/java/com/threeping/mudium_backend/MudiumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.threeping.mudium_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudiumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudiumBackendApplication.class, args);
+	}
+
+}
