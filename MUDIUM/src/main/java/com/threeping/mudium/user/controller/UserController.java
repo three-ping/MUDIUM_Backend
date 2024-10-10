dc29fdfb15342b2569e39951696f27b915666f70
1etterh <1etterh.dev@gmail.com>
2024-10-10 17:54:35 +0900

    feat: send email

--- MUDIUM/src/main/java/com/threeping/mudium/user/controller/UserController.java
+++ MUDIUM/src/main/java/com/threeping/mudium/user/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.threeping.mudium.user.controller;
 
 import com.threeping.mudium.common.ResponseDTO;
+import com.threeping.mudium.common.exception.CommonException;
+import com.threeping.mudium.common.exception.ErrorCode;
 import com.threeping.mudium.user.aggregate.dto.UserDTO;
 import com.threeping.mudium.user.aggregate.vo.RequestRegistUserVO;
 import com.threeping.mudium.user.aggregate.vo.ResponseUserVO;
+import com.threeping.mudium.user.service.EmailVerificationService;
 import com.threeping.mudium.user.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +19,14 @@ public class UserController {
     private final Environment env;
     private final ModelMapper modelMapper;
     private final UserService userService;
+    private final EmailVerificationService emailVerificationService;
 
     @Autowired
-    public UserController(Environment env, ModelMapper modelMapper, UserService userService) {
+    public UserController(Environment env, ModelMapper modelMapper, UserService userService, EmailVerificationService emailVerificationService) {
         this.env = env;
         this.modelMapper = modelMapper;
         this.userService = userService;
+        this.emailVerificationService = emailVerificationService;
     }
     @GetMapping("/health")
     public String healthCheck(){
@@ -30,11 +35,26 @@ public class UserController {
 
     @PostMapping("/normal")
     public ResponseDTO<?> registNormalUser(@RequestBody RequestRegistUserVO newUser){
+
+        /* 설명. 이메일 인증 */
+        emailVerificationService.sendVerificationCode(newUser.getEmail());
+
         UserDTO savedUserDTO = userService.registUser(newUser);
 
         ResponseUserVO responseUser = modelMapper.map(savedUserDTO, ResponseUserVO.class);
 
         return ResponseDTO.ok(responseUser);
     }
+
+    @PostMapping("/verify-code")
+    public ResponseDTO<?> verifyEmailCode(@RequestParam String email, @RequestParam String code){
+        boolean isVerified = emailVerificationService.verifyCode(email, code);
+        if(isVerified){
+            return ResponseDTO.ok("이메일 인증이 완료되었습니다.");
+        } else{
+            return ResponseDTO.fail(new CommonException(ErrorCode.INVALID_VERIFICATION_CODE));
+        }
+    }
+
 
 }
