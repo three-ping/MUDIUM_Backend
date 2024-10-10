965fa8c627e3f4a31879e5ac8beea8bcbd2dceb5
1etterh <1etterh.dev@gmail.com>
2024-10-10 10:26:28 +0900

    Refactor: Add MUDIUM Project

---
+++ MUDIUM/src/main/java/com/ohgiraffers/mudium/MudiumApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.mudium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MudiumApplication.class, args);
+    }
+
+}
---
+++ MUDIUM/src/test/java/com/ohgiraffers/mudium/MudiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.mudium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
