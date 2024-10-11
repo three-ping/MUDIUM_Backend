37095c7df0259dea608eb60f300a586a92818488
dongkh9 <dongkh99@gamil.com>
2024-10-11 15:38:37 +0900

    refactor: common 오류 핸들러 수정
    오류 핸들러 베이스패키지 수정

--- MUDIUM/src/main/java/com/threeping/mudium/common/exception/GlobalExceptionHandler.java
+++ MUDIUM/src/main/java/com/threeping/mudium/common/exception/GlobalExceptionHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 //필기. 해당 패키지에서 에러 발생시 작동하는 Handler
-@RestControllerAdvice(basePackages = "com.springcooler.sgma.user")
+@RestControllerAdvice(basePackages = "com.threeping.mudium")
 public class GlobalExceptionHandler {
 
     // 지원되지 않는 HTTP 메소드를 사용할 때 발생하는 예외
