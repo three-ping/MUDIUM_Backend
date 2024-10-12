56cb47975512b6eb7023ee6f64971cef56ec0274
이효진 <jin12lee@naver.com>
2024-10-12 21:34:50 +0900

    feat: 입문 작품 추천 수정 기능

--- MUDIUM/src/main/java/com/threeping/mudium/guidebook/controller/RecommendedController.java
+++ MUDIUM/src/main/java/com/threeping/mudium/guidebook/controller/RecommendedController.java
@@ -2,11 +2,10 @@ package com.threeping.mudium.guidebook.controller;
 
 import com.threeping.mudium.common.ResponseDTO;
 import com.threeping.mudium.guidebook.dto.request.RecommendedRequestDTO;
-import com.threeping.mudium.guidebook.dto.response.RecommendedResponseDTO;
 import com.threeping.mudium.guidebook.entity.RecommendedMusical;
 import com.threeping.mudium.guidebook.service.RecommendedService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,27 +17,32 @@ public class RecommendedController {
     private final RecommendedService recommendedService;
 
     @Autowired
-    public RecommendedController(RecommendedService recommendedService) {
+    public RecommendedController ( RecommendedService recommendedService ) {
         this.recommendedService = recommendedService;
     }
 
     @PostMapping("/post")
-    public ResponseDTO<?> createRecommended(@RequestBody RecommendedRequestDTO recommendedRequestDTO){
-        RecommendedMusical createRecommend = recommendedService.createRecommended(recommendedRequestDTO);
-        return ResponseDTO.ok(createRecommend);
+    public ResponseDTO<?> createRecommended ( @RequestBody RecommendedRequestDTO recommendedRequestDTO ) {
+        RecommendedMusical createRecommend = recommendedService.createRecommended ( recommendedRequestDTO );
+        return ResponseDTO.ok ( createRecommend );
     }
 
-//    @PutMapping("/update/{recommendedId}")
-//    public
+    @PutMapping("/update/{recommendedId}")
+    public ResponseDTO<?> modifyRecommended ( @PathVariable Long recommendedId,
+                                              @RequestBody RecommendedRequestDTO recommendedRequestDTO ) {
+        recommendedService.updateRecommended ( recommendedRequestDTO, recommendedId );
+
+        return ResponseDTO.ok ( recommendedId );
+    }
 
     @GetMapping("")
-    public ResponseDTO<?> findRecommendedList(){
-        List<RecommendedRequestDTO> recommended = recommendedService.findRecommendedList();
-        return ResponseDTO.ok(recommended);
+    public ResponseDTO<?> findRecommendedList () {
+        List<RecommendedRequestDTO> recommended = recommendedService.findRecommendedList ();
+        return ResponseDTO.ok ( recommended );
     }
 
     @GetMapping("/detail/{recommendedId}")
-    public ResponseDTO<?> findByRecommendedId(@PathVariable Long recommendedId){
-        return ResponseDTO.ok(recommendedService.findByRecommendedId(recommendedId));
+    public ResponseDTO<?> findByRecommendedId ( @PathVariable Long recommendedId ) {
+        return ResponseDTO.ok ( recommendedService.findByRecommendedId ( recommendedId ) );
     }
 }
