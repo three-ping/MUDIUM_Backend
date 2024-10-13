65d1cded01cdd6bb907e2fe4977c08822b6bff92
1etterh <1etterh.dev@gmail.com>
2024-10-13 12:18:21 +0900

    feat: processKakaoUser

--- MUDIUM/src/main/java/com/threeping/mudium/user/aggregate/vo/OAuth2LoginVO.java
+++ MUDIUM/src/main/java/com/threeping/mudium/user/aggregate/vo/OAuth2LoginVO.java
@@ -4,8 +4,10 @@ import lombok.Data;
 
 @Data
 public class OAuth2LoginVO {
+    private String id;
     private String userName;
     private String email;
+    private String pwd;
     private String accessToken;
     private String refreshToken;
 }
--- MUDIUM/src/main/java/com/threeping/mudium/user/controller/OAuth2Controller.java
+++ MUDIUM/src/main/java/com/threeping/mudium/user/controller/OAuth2Controller.java
@@ -1,6 +1,9 @@
 package com.threeping.mudium.user.controller;
 
 import com.threeping.mudium.common.ResponseDTO;
+import com.threeping.mudium.user.aggregate.dto.UserDTO;
+import com.threeping.mudium.user.aggregate.vo.OAuth2LoginVO;
+import com.threeping.mudium.user.service.OAuth2UserService;
 import com.threeping.mudium.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -15,18 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuth2Controller {
     private ModelMapper modelMapper;
     private UserService userService;
+    private OAuth2UserService oAuth2UserService;
 
     @Autowired
-    public OAuth2Controller(ModelMapper modelMapper, UserService userService) {
+    public OAuth2Controller(ModelMapper modelMapper
+            , UserService userService
+            , OAuth2UserService oAuth2UserService) {
         this.modelMapper = modelMapper;
         this.userService = userService;
+        this.oAuth2UserService = oAuth2UserService;
     }
 
     @GetMapping("/kakao")
     public ResponseDTO<?> kakaoLogin(@RequestParam String code) {
         log.info("넘어온 카카오 코드: {}", code);
 
-        return null;
+        OAuth2LoginVO user = oAuth2UserService.processKakaoUser(code);
+        log.info("userDTO: {}", user);
+        return ResponseDTO.ok(user);
 
 
     }
