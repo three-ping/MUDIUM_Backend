ded7e03990273d26fef91136c57570f7e6bdd3d7
siu98 <parancandy@naver.com>
2024-10-14 17:24:54 +0900

    feat: 회원별 커스텀 티켓 조회

--- MUDIUM/src/main/java/com/threeping/mudium/customticket/repository/UserCustomTicketRepository.java
+++ MUDIUM/src/main/java/com/threeping/mudium/customticket/repository/UserCustomTicketRepository.java
@@ -4,5 +4,8 @@ import com.threeping.mudium.customticket.aggregate.entity.UserCustomTicketEntity
 import com.threeping.mudium.customticket.aggregate.entity.UserCustomTicketId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserCustomTicketRepository extends JpaRepository<UserCustomTicketEntity, UserCustomTicketId> {
+    List<UserCustomTicketEntity> findByUserId(Long userId);
 }
