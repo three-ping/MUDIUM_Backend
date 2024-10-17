b74c90b68b142f5c4a6de1fce1133e1abfb9d86a
dongkh9 <dongkh99@gamil.com>
2024-10-17 15:01:52 +0900

    feat: 문의게시글 수정 및 삭제 기능 추가
    문의 게시글 수정 및 삭제 기능 추가

--- MUDIUM/src/main/java/com/threeping/mudium/inquiry/controller/InquiryController.java
+++ MUDIUM/src/main/java/com/threeping/mudium/inquiry/controller/InquiryController.java
@@ -5,6 +5,7 @@ import com.threeping.mudium.common.ResponseDTO;
 import com.threeping.mudium.inquiry.dto.CreateInquiryDTO;
 import com.threeping.mudium.inquiry.dto.InquiryDetailDTO;
 import com.threeping.mudium.inquiry.dto.InquiryListDTO;
+import com.threeping.mudium.inquiry.dto.UpdateInquiryDTO;
 import com.threeping.mudium.inquiry.service.InquiryService;
 import com.threeping.mudium.user.aggregate.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +56,25 @@ public class InquiryController {
         user.setUserId(userId);
         createInquiryDTO.setUser(user);
         inquiryService.createInquiry(createInquiryDTO);
+
+        return ResponseDTO.ok(null);
+    }
+
+    @PutMapping("{userId}/{inquiryId}")
+    private ResponseDTO<?> updateInquiry(@PathVariable Long userId,
+                                         @PathVariable Long inquiryId,
+                                         @RequestBody UpdateInquiryDTO updateInquiryDTO){
+        updateInquiryDTO.setUserId(userId);
+        updateInquiryDTO.setInquiryId(inquiryId);
+        inquiryService.updateInquiry(updateInquiryDTO);
+
+        return ResponseDTO.ok(null);
+    }
+
+    @DeleteMapping("{userId}/{inquiryId}")
+    private ResponseDTO<?> deleteInquiry(@PathVariable Long userId,
+                                         @PathVariable Long inquiryId){
+        inquiryService.deleteInquiry(userId,inquiryId);
 
         return ResponseDTO.ok(null);
     }
